123205f78918f62d9fd5c9f372648f2452176ea6
Shaunak <shaunak206107@gmail.com>
2026-06-03 19:35:06 +0530

    Updated config part 2

--- includes/config.php
+++ includes/config.php
@@ -14,7 +14,7 @@ $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $password = getenv('MYSQLPASSWORD');
 $database = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
+$port = (int)getenv('MYSQLPORT');
 
 // Check if credentials are loaded
 if (!$host || !$user || !$password || !$database) {
